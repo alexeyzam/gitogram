4490ff5920a71e68b7ebe7e403ced945f3adce00
Alexey <alexey.zamorin@gmail.com>
2022-10-30 23:54:02 +0500

    Added test for Toggler

--- tests/toggler.test.js
+++ tests/toggler.test.js
@@ -2,8 +2,10 @@ import {shallowMount} from "@vue/test-utils";
 import Toggler from "@/components/Toggler";
 
 it("Toggler",async ()=>{
-    const wrapper = shallowMount(Toggler)
+
+    const $emit = jest.fn()
+    const wrapper = shallowMount(Toggler,{mocks: {$emit}})
     expect(wrapper.find('div').find('span').text()).toBe('Show issues')
     await wrapper.find('.issue-button').trigger('click')
-    expect(wrapper.find('div').find('span').text()).toBe('Show issues')
+    expect(wrapper.find('div').find('span').text()).toBe('Hide issues')
 })
